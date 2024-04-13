005751758104d3c49d2cbc6e1bab203bb34c37b7
Pedro Veras <veraspedro1608@gmail.com>
2024-04-13 15:57:57 -0300

    feat: abstracting entity creation

--- src/domain/entities/category.ts
+++ src/domain/entities/category.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { Optional } from "../../core/types/optional"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface CategoryProps {
   name: string
   slug: Slug
   companyId: string
   isActive: boolean
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Category extends Entity<CategoryProps> {
@@ -23,5 +27,25 @@ export class Category extends Entity<CategoryProps> {
 
   get isActive() {
     return this.props.isActive
+  }
+  
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  static create(
+    props: Optional<CategoryProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const category = new Category({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return category
   }
 }
--- src/domain/entities/company.ts
+++ src/domain/entities/company.ts
@@ -1,11 +1,14 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface CompanyProps {
   name: string
   email: string
   contact: string
-  createdAt: string
   responsible: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Company extends Entity<CompanyProps> {
@@ -27,5 +30,17 @@ export class Company extends Entity<CompanyProps> {
 
   get responsible() {
     return this.props.responsible
+  }
+
+  static create(
+    props: Optional<CompanyProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const company = new Company({
+      ...props,
+      createdAt: new Date()
+    }, id)
+
+    return company
   }
 }
