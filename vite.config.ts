e403ec77a472bba774e9bbc0d2a6cc54d2e042df
Pedro Veras <veraspedro1608@gmail.com>
2024-04-15 23:03:52 -0300

    featr: initial infrastructure structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
+  plugins: [
+    tsConfigPaths(),
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
   test: {
-    globals: true
-  }
-})
+    globals: true,
+  },
+});
