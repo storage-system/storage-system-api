cb0c677595e2453044df32b65c88359b70329d0b
Pedro Veras <veraspedro1608@gmail.com>
2024-04-19 20:51:02 -0300

    feat: generting jwt token

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
-  JWT_SECRET: z.string(),
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
   DATABASE_URL: z.string().url(),
   PORT: z.coerce.number().optional().default(3333),
 })
