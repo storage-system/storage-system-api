66525a2dacb0c82461506a2880cdbd870b526f32
Pedro Veras <veraspedro1608@gmail.com>
2024-04-20 16:46:01 -0300

    feat: implementing category repositories in prisma

--- src/infra/database/prisma/repositories/prisma-categories-repository.ts
+++ src/infra/database/prisma/repositories/prisma-categories-repository.ts
@@ -2,26 +2,72 @@ import { PaginationProps, Pagination } from "@/core/entities/pagination";
 import { CategoriesRepository } from "@/domain/application/category/categories-repository";
 import { Category } from "@/domain/enterprise/category/category";
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { PrismaCategoryMapper } from "../mappers/prisma-category-mapper";
 
 @Injectable()
 export class PrismaCategoriesRepository implements CategoriesRepository {
-  findById(id: string): Promise<Category | null> {
-    throw new Error("Method not implemented.");
+  constructor(private prisma: PrismaService) { }
+
+  async findById(id: string): Promise<Category | null> {
+    const category = await this.prisma.category.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if (!category) {
+      return null
+    }
+
+    return PrismaCategoryMapper.toDomain(category)
   }
 
-  findAll(params: PaginationProps<Category>): Promise<Pagination<Category>> {
-    throw new Error("Method not implemented.");
+  async findAll(params: PaginationProps<Category>): Promise<Pagination<Category>> {
+    const categories = await this.prisma.category.findMany({
+      orderBy: {
+        slug: 'asc'
+      },
+      take: params.perPage,
+      skip: (params.page - 1) * params.perPage
+    })
+
+    const totalCategories = await this.prisma.category.count()
+
+    return new Pagination({
+      page: params.page,
+      perPage: params.perPage,
+      items: categories.map(PrismaCategoryMapper.toDomain),
+      total: totalCategories,
+    })
   }
 
-  create(category: Category): Promise<void> {
-    throw new Error("Method not implemented.");
+  async create(category: Category): Promise<void> {
+    const data = PrismaCategoryMapper.toPersistence(category)
+
+    await this.prisma.category.create({
+      data,
+    })
   }
 
-  save(category: Category): Promise<void> {
-    throw new Error("Method not implemented.");
+  async save(category: Category) {
+    const data = PrismaCategoryMapper.toPersistence(category)
+
+    await this.prisma.category.update({
+      where: {
+        id: data.id,
+      },
+      data
+    })
   }
 
-  delete(category: Category): Promise<void> {
-    throw new Error("Method not implemented.");
+  async delete(category: Category) {
+    const data = PrismaCategoryMapper.toPersistence(category)
+
+    await this.prisma.category.delete({
+      where: {
+        id: data.id,
+      }
+    })
   }
 }
