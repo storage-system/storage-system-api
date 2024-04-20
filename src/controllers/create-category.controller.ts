e0c646860ff786c39012fde907f925e64f9668bf
Pedro Veras <veraspedro1608@gmail.com>
2024-04-20 13:07:06 -0300

    feat: creating authentication decorator

--- src/controllers/create-category.controller.ts
+++ src/controllers/create-category.controller.ts
@@ -1,3 +1,5 @@
+import { CurrentUser } from '@/auth/current-user-decorator'
+import { UserPayload } from '@/auth/jwt.strategy'
 import { Controller, Post, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
@@ -7,7 +9,8 @@ export class CreateCategoryController {
   constructor() { }
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user.sub)
     return 'ok'
   }
 }
