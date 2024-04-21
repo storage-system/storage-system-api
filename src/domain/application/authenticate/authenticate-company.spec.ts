453b3533fd6a113347057744993ebeae69ec8173
Pedro Veras <veraspedro1608@gmail.com>
2024-04-21 11:11:04 -0300

    feat: authenticate company use case test

---
+++ src/domain/application/authenticate/authenticate-company.spec.ts
@@ -0,0 +1,45 @@
+import { FakeEncrypter } from "test/cryptography/fake-encrypter"
+import { FakeHasher } from "test/cryptography/fake-hasher"
+import { InMemoryCompaniesRepository } from "test/repositories/in-memory-companies-repository"
+import { AuthenticateCompanyUseCase } from "./authenticate-company"
+import { makeCompany } from "test/factories/make-company"
+
+
+let inMemoryCompaniesRepository: InMemoryCompaniesRepository
+let fakeHasher: FakeHasher
+let encrypter: FakeEncrypter
+
+let sut: AuthenticateCompanyUseCase
+
+describe('Authenticate Student', () => {
+  beforeEach(() => {
+    inMemoryCompaniesRepository = new InMemoryCompaniesRepository()
+    fakeHasher = new FakeHasher()
+    encrypter = new FakeEncrypter()
+
+    sut = new AuthenticateCompanyUseCase(
+      inMemoryCompaniesRepository,
+      fakeHasher,
+      encrypter,
+    )
+  })
+
+  it('should be able to authenticate a company', async () => {
+    const company = makeCompany({
+      email: 'johndoeeletronics@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryCompaniesRepository.items.push(company)
+
+    const result = await sut.execute({
+      email: company.email,
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
