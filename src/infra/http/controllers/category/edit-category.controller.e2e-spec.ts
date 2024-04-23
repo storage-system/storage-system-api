ef3c771faf5e6d03577d9c9bbe3e9c11e9c795f9
Pedro Veras <veraspedro1608@gmail.com>
2024-04-22 21:14:47 -0300

    create edit category controller e2e test

---
+++ src/infra/http/controllers/category/edit-category.controller.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { AppModule } from "@/infra/app.module"
+import { DatabaseModule } from "@/infra/database/database.module"
+import { INestApplication } from "@nestjs/common"
+import { JwtService } from "@nestjs/jwt"
+import { Test } from "@nestjs/testing"
+import { CategoryFactory } from "test/factories/make-category"
+import { CompanyFactory } from "test/factories/make-company"
+import request from 'supertest'
+import { PrismaService } from "@/infra/database/prisma/prisma.service"
+
+describe('Edit category (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let companyFactory: CompanyFactory
+  let categoryFactory: CategoryFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [CompanyFactory, CategoryFactory],
+    }).compile()
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+    companyFactory = moduleRef.get(CompanyFactory)
+    categoryFactory = moduleRef.get(CategoryFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[PATCH] /categories/:id', async () => {
+    const company = await companyFactory.makePrismaCompany()
+
+    const accessToken = jwt.sign({ sub: company.id.toString() })
+
+    const category = await categoryFactory.makePrismaCategory({
+      name: 'category-01',
+      companyId: company.id,
+      isActive: true,
+    })
+
+    const categoryId = category.id
+
+    const updateCategory = {
+      name: 'new-category-01',
+      isActive: true,
+    }
+
+    const response = await request(app.getHttpServer())
+      .patch(`/categories/${categoryId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send(updateCategory)
+
+    expect(response.statusCode).toBe(204)
+
+    const categoryOnDatabase = await prisma.category.findFirst({
+      where: {
+        name: updateCategory.name,
+        isActive: updateCategory.isActive,
+      }
+    })
+
+    expect(categoryOnDatabase).toBeTruthy()
+  })
+})
