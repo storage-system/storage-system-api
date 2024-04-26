ea2fc2f4366cbd250c73fab706e1209a6e46569f
Pedro Veras <pedro.veras@kingspanisoeste.com.br>
2024-04-26 13:13:11 -0300

    refactor: changing controller folder name to modules

--- src/infrastructure/http/http.module.ts
+++ src/infrastructure/http/http.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "../database/database.module";
 import { CryptographyModule } from "../cryptography/cryptography.module";
-import { CategoryModule } from "./controllers/category/category.module";
-import { CompanyModule } from "./controllers/company/company.module";
-import { AuthenticateModule } from "./controllers/authenticate/authenticate.module";
+import { CategoryModule } from "./modules/category/category.module";
+import { CompanyModule } from "./modules/company/company.module";
+import { AuthenticateModule } from "./modules/authenticate/authenticate.module";
 
 @Module({
   imports: [
--- src/infrastructure/http/modules/company/company.module.ts
+++ src/infrastructure/http/modules/company/company.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 
 import { CreateCompanyUseCase } from "@/domain/application/company/use-cases/create-company";
 import { DatabaseModule } from "@/infrastructure/database/database.module";
-import { CreateAccountController } from "./create-account.controller";
+import { CreateAccountController } from "./company.controller";
 import { CryptographyModule } from "@/infrastructure/cryptography/cryptography.module";
 
 @Module({
