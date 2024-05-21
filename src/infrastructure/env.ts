ad52eca7ccb218f9e1482d8b6a38332343d9e303
PedroVeras18 <veraspedro1608@gmail.com>
2024-05-21 07:58:58 -0300

    feat: add more environment variables

--- src/infrastructure/env.ts
+++ src/infrastructure/env.ts
@@ -4,6 +4,10 @@ export const envSchema = z.object({
   JWT_PRIVATE_KEY: z.string(),
   JWT_PUBLIC_KEY: z.string(),
   DATABASE_URL: z.string().url(),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
+  DB_NAME: z.string(),
+  DB_HOST: z.string(),
   PORT: z.coerce.number().optional().default(3333),
 })
 
