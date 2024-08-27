8ad3d8971c9130ddfbff81908a3bdd36348e0350
PedroVeras18 <veraspedro1608@gmail.com>
2024-08-27 10:55:12 -0300

    feat: ✨ configuration domain

---
+++ src/domain/enterprise/configuration/configuration-repository.ts
@@ -0,0 +1,4 @@
+import { Repository } from '@/core/repository'
+import { Configuration } from './configuration'
+
+export abstract class ConfigurationRepository extends Repository<Configuration> { }
---
+++ src/domain/enterprise/configuration/configuration.ts
@@ -0,0 +1,128 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Entity } from "@/core/entities/entity"
+import { Optional } from "@/core/types/optional"
+
+export enum ReportFrequency {
+  DIARY = 'diary',
+  WEEKLY = 'weekly',
+  MONTH = 'month',
+  YEAR = 'year'
+}
+
+export interface ConfigurationProps {
+  userId: UniqueEntityID
+  companyId: UniqueEntityID
+  daysBeforeOldStock: number
+  warningDays: number
+  emailNotification: boolean
+  systemNotification: boolean
+  autoDiscardAfterExpiration: boolean
+  freeShippingOnOldStock: boolean
+  reportFrequency: ReportFrequency
+  createdAt: Date
+  updatedAt?: Date | null
+  deletedAt?: Date | null
+}
+
+export class Configuration extends Entity<ConfigurationProps> {
+  static create(
+    props: Optional<ConfigurationProps, 'createdAt' | 'daysBeforeOldStock' | 'warningDays'>,
+    id?: UniqueEntityID,
+  ) {
+    const configuration = new Configuration(
+      {
+        daysBeforeOldStock: 30,
+        warningDays: 15,
+        createdAt: new Date(),
+        ...props,
+      },
+      id,
+    )
+
+    return configuration
+  }
+
+  update(aConfiguration: Partial<ConfigurationProps>) {
+    this.props.daysBeforeOldStock = aConfiguration.daysBeforeOldStock !== undefined
+      ? aConfiguration.daysBeforeOldStock
+      : this.daysBeforeOldStock;
+
+    this.props.warningDays = aConfiguration.warningDays !== undefined
+      ? aConfiguration.warningDays
+      : this.warningDays;
+
+    this.props.emailNotification = aConfiguration.emailNotification !== undefined
+      ? aConfiguration.emailNotification
+      : this.emailNotification;
+
+    this.props.systemNotification = aConfiguration.systemNotification !== undefined
+      ? aConfiguration.systemNotification
+      : this.systemNotification;
+
+    this.props.autoDiscardAfterExpiration = aConfiguration.autoDiscardAfterExpiration !== undefined
+      ? aConfiguration.autoDiscardAfterExpiration
+      : this.autoDiscardAfterExpiration;
+
+    this.props.freeShippingOnOldStock = aConfiguration.freeShippingOnOldStock !== undefined
+      ? aConfiguration.freeShippingOnOldStock
+      : this.freeShippingOnOldStock;
+
+    this.props.reportFrequency = aConfiguration.reportFrequency !== undefined
+      ? aConfiguration.reportFrequency
+      : this.reportFrequency;
+
+    this.touch()
+  }
+
+  get userId() {
+    return this.props.userId.toString()
+  }
+
+  get companyId() {
+    return this.props.companyId.toString()
+  }
+
+  get daysBeforeOldStock() {
+    return this.props.daysBeforeOldStock
+  }
+
+  get warningDays() {
+    return this.props.warningDays
+  }
+
+  get emailNotification() {
+    return this.props.emailNotification
+  }
+
+  get systemNotification() {
+    return this.props.systemNotification
+  }
+
+  get autoDiscardAfterExpiration() {
+    return this.props.autoDiscardAfterExpiration
+  }
+
+  get freeShippingOnOldStock() {
+    return this.props.freeShippingOnOldStock
+  }
+
+  get reportFrequency() {
+    return this.props.reportFrequency
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get deletedAt() {
+    return this.props.deletedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+}
