cda159b052ec35dd76d288c2a7eaf5850d461de1
PedroVeras18 <veraspedro1608@gmail.com>
2024-10-05 18:47:50 -0300

    feat: ✨ minio file storage gateway

--- src/infrastructure/env.ts
+++ src/infrastructure/env.ts
@@ -9,6 +9,14 @@ export const envSchema = z.object({
   DB_NAME: z.string(),
   DB_HOST: z.string(),
   PORT: z.coerce.number().optional().default(3333),
+  MINIO_USER: z.string(),
+  MINIO_PASSWORD: z.string(),
+  MINIO_ENDPOINT: z.string(),
+  MINIO_PORT: z.coerce.number().optional().default(9000),
+  MINIO_ACCESS_KEY: z.string(),
+  MINIO_SECRET_KEY: z.string(),
+  MINIO_USE_SSL: z.coerce.boolean(),
+  MINIO_BUCKET_NAME: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
---
+++ src/infrastructure/services/file-storage/minio.file-storage.gateway.ts
@@ -0,0 +1,59 @@
+import { FileStorageGateway } from '@/domain/enterprise/file/file-storage.gateway'
+import { EnvService } from '@/infrastructure/env/env.service'
+import { Inject, Injectable } from '@nestjs/common'
+import { ReadStream } from 'fs'
+import * as Minio from 'minio'
+import { Readable } from 'stream'
+
+@Injectable()
+export class MinioFileStorageGateway implements FileStorageGateway {
+  private minioClient: Minio.Client
+  private bucketName: string
+
+  constructor(
+    @Inject(EnvService)
+    private readonly env: EnvService
+  ) {
+    this.minioClient = new Minio.Client({
+      endPoint: this.env.get('MINIO_ENDPOINT'),
+      port: Number(this.env.get('MINIO_PORT')),
+      useSSL: this.env.get('MINIO_USE_SSL') === 'true',
+      accessKey: this.env.get('MINIO_ACCESS_KEY'),
+      secretKey: this.env.get('MINIO_SECRET_KEY')
+    })
+    
+    this.bucketName = this.env.get('MINIO_BUCKET_NAME')
+  }
+
+  async createBucketIfNotExists() {
+    const bucketExists = await this.minioClient.bucketExists(this.bucketName)
+    if (!bucketExists) {
+      await this.minioClient.makeBucket(this.bucketName, 'eu-west-1')
+    }
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    const fileName = `${Date.now()}-${file.originalname}`
+
+    await this.minioClient.putObject(
+      this.bucketName,
+      fileName,
+      file.buffer,
+      file.size
+    )
+    
+    return fileName
+  }
+
+  async getFileUrl(fileName: string) {
+    return await this.minioClient.presignedUrl('GET', this.bucketName, fileName)
+  }
+
+  async downloadFile(fileName: string): Promise<Readable> {
+    return await this.minioClient.getObject(this.bucketName, fileName)
+  }
+
+  async deleteFile(fileName: string) {
+    await this.minioClient.removeObject(this.bucketName, fileName)
+  }
+}
